c6a1b4e072f7ef52f7726e2a8f5401055aa6fa10
Yagiz Ozcan <yagizozcan.bs@gmail.com>
2023-12-03 14:52:32 +0300

    done

--- wave_function/Assets/ChunkCreator.cs
+++ wave_function/Assets/ChunkCreator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class ChunkCreator : MonoBehaviour
 {
@@ -63,6 +64,7 @@ public class ChunkCreator : MonoBehaviour
                 superPositionObj.GetComponent<superPositionsData>().pos = new Vector2(i,j);
                 superPositionObj.GetComponent<superPositionsData>().tile = null;
                 superPositionObj.GetComponent<superPositionsData>().currentTiles = new List<Tiles>();
+                superPositionObj.tag = "Objs";
                 foreach(Tiles tile in allTiles)
                 {
                     superPositionObj.GetComponent<superPositionsData>().currentTiles.Add(tile);
@@ -80,13 +82,14 @@ public class ChunkCreator : MonoBehaviour
         superPositionUIObj.transform.SetParent(canvas.transform);
         superPositionUIObj.AddComponent<RectTransform>();
         superPositionUIObj.GetComponent<RectTransform>().localScale = Vector3.one;
-        superPositionUIObj.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / gridSize, Screen.height / gridSize);
+        superPositionUIObj.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.height / gridSize, Screen.height / gridSize);
         superPositionUIObj.AddComponent<GridLayoutGroup>();
         superPositionUIObj.GetComponent<GridLayoutGroup>().spacing = Vector2.one * 5;
         Vector2 cellSize = Vector2.one * ((superPositionUIObj.GetComponent<RectTransform>().sizeDelta.x / NxN) - superPositionUIObj.GetComponent<GridLayoutGroup>().spacing.x);
         superPositionUIObj.GetComponent<GridLayoutGroup>().cellSize = cellSize;
         superPositionUIObj.GetComponent<GridLayoutGroup>().childAlignment = TextAnchor.UpperCenter;
         superPositionUIObj.transform.position = superPositionObj.transform.position;
+        superPositionUIObj.tag = "Objs";
         int cellCount = NxN * NxN;
         for(int i = 0; i < cellCount; i++)
         {
@@ -213,8 +216,8 @@ public class ChunkCreator : MonoBehaviour
             int NxN = CalculateNxN(superPositionObjs[(int)data.pos.x][(int)data.pos.y + 1].GetComponent<superPositionsData>().currentTiles.Count);
             RearrangeSuperPositionUI(superPositionUIObjs[(int)data.pos.x][(int)data.pos.y + 1], superPositionObjs[(int)data.pos.x][(int)data.pos.y + 1], NxN, superPositionObjs[(int)data.pos.x][(int)data.pos.y + 1].GetComponent<superPositionsData>().currentTiles);
         }*/
-        superPositionObj.GetComponent<SpriteRenderer>().sprite = EventSystem.current.currentSelectedGameObject.GetComponent<Image>().sprite;
-        superPositionObj.transform.localScale = 16*Vector2.one;
+        superPositionObj.GetComponent<SpriteRenderer>().sprite = superPositionObj.GetComponent<superPositionsData>().tile.tileSprite;
+        superPositionObj.transform.localScale =  Vector2.one * 64 / gridSize;
         superPositionUIObjs[(int)data.pos.x][(int)data.pos.y].SetActive(false);
     }
     public int CalculateNxN(int length)
@@ -243,5 +246,94 @@ public class ChunkCreator : MonoBehaviour
         }
 
         return true;
+    }
+
+    public void QuickSolve()
+    {
+        List<GameObject> openSuperPositionObjs = new List<GameObject>();
+        List<GameObject> closedSuperPositionObjs = new List<GameObject>();
+        openSuperPositionObjs.Add(superPositionObjs[Random.Range(0, gridSize)][Random.Range(0, gridSize)]);
+        int i = 0;
+        int lowestEntropy = allTiles.Length + 1;
+        while (openSuperPositionObjs.Count != 0)
+        {
+            lowestEntropy = allTiles.Length + 1;
+            GameObject currentSuperObj = null;
+            List<GameObject> equalEntropies = new List<GameObject>();
+            foreach (GameObject superPositionObjInOpen in openSuperPositionObjs)
+            {
+                if (superPositionObjInOpen.GetComponent<superPositionsData>().currentTiles.Count == lowestEntropy)
+                {
+                    equalEntropies.Add(superPositionObjInOpen);
+                }
+                if (superPositionObjInOpen.GetComponent<superPositionsData>().currentTiles.Count < lowestEntropy)
+                {
+                    currentSuperObj = superPositionObjInOpen;
+                    lowestEntropy = superPositionObjInOpen.GetComponent<superPositionsData>().currentTiles.Count;
+                    equalEntropies.Clear();
+                }
+            }
+            if(equalEntropies.Count != 0)
+            {
+                currentSuperObj = equalEntropies[Random.Range(0,equalEntropies.Count)];
+            }
+            superPositionsData data = currentSuperObj.GetComponent<superPositionsData>();
+            data.tile = data.currentTiles[Random.Range(0, data.currentTiles.Count)];
+            CheckSuperPositionNeighbours(currentSuperObj);
+            closedSuperPositionObjs.Add(currentSuperObj);
+            openSuperPositionObjs.Remove(currentSuperObj);
+            if (data.pos.x - 1 >= 0 && superPositionObjs[(int)data.pos.x - 1][(int)data.pos.y].GetComponent<superPositionsData>().tile == null && !closedSuperPositionObjs.Contains(superPositionObjs[(int)data.pos.x - 1][(int)data.pos.y]))
+            {
+                if(!openSuperPositionObjs.Contains(superPositionObjs[(int)data.pos.x - 1][(int)data.pos.y]))
+                {
+                    openSuperPositionObjs.Add(superPositionObjs[(int)data.pos.x - 1][(int)data.pos.y]);
+                }
+            }
+            if (data.pos.x + 1 < superPositionObjs.Length && superPositionObjs[(int)data.pos.x + 1][(int)data.pos.y].GetComponent<superPositionsData>().tile == null && !closedSuperPositionObjs.Contains(superPositionObjs[(int)data.pos.x + 1][(int)data.pos.y]))
+            {
+                if (!openSuperPositionObjs.Contains(superPositionObjs[(int)data.pos.x + 1][(int)data.pos.y]))
+                {
+                    openSuperPositionObjs.Add(superPositionObjs[(int)data.pos.x + 1][(int)data.pos.y]);
+                }
+            }
+            if (data.pos.y - 1 >= 0 && superPositionObjs[(int)data.pos.x][(int)data.pos.y - 1].GetComponent<superPositionsData>().tile == null && !closedSuperPositionObjs.Contains(superPositionObjs[(int)data.pos.x][(int)data.pos.y - 1]))
+            {
+                if (!openSuperPositionObjs.Contains(superPositionObjs[(int)data.pos.x][(int)data.pos.y - 1]))
+                {
+                    openSuperPositionObjs.Add(superPositionObjs[(int)data.pos.x][(int)data.pos.y - 1]);
+                }
+            }
+            if (data.pos.y + 1 < superPositionObjs.Length && superPositionObjs[(int)data.pos.x][(int)data.pos.y + 1].GetComponent<superPositionsData>().tile == null && !closedSuperPositionObjs.Contains(superPositionObjs[(int)data.pos.x][(int)data.pos.y + 1]))
+            {
+                if (!openSuperPositionObjs.Contains(superPositionObjs[(int)data.pos.x][(int)data.pos.y + 1]))
+                {
+                    openSuperPositionObjs.Add(superPositionObjs[(int)data.pos.x][(int)data.pos.y + 1]);
+                }
+            }
+            i++;
+            if(i > 1000)
+            {
+                break;
+            }
+        }
+    }
+
+    public void loadScene(int loadScene)
+    {
+        SceneManager.LoadScene(loadScene);
+    }
+
+    public void SetGridSize(InputField value)
+    {
+        if(value.text != null)
+        {
+            gridSize = int.Parse(value.text);
+            GameObject[] objs = GameObject.FindGameObjectsWithTag("Objs");
+            foreach (GameObject obj in objs)
+            {
+                Destroy(obj);
+            }
+            CreateSuperPositions();
+        }
     }
 }
